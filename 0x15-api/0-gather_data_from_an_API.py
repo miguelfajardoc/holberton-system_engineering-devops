f0871036d3c42ea9f7de0f717427351e139b85c6
miguelfajardoc <775@holbertonschool.com>
2019-09-17 18:59:31 +0000

    add data from api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import json
+    from requests import get
+    from sys import argv
+
+
+    user = get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    task = get("https://jsonplaceholder.typicode.com/todos/", params={'userId': argv[1]})
+    user = user.json()
+    task = task.json()
+
+    total = 0
+    done = 0
+    tasks = []
+
+    for t in task:
+        total += 1
+        if t["completed"] is True:
+            done += 1
+            tasks.append(t["title"])
+
+    print("Employee {} is done with task({:d}/{:d})".format(user["name"],
+                                                            done, total))
+    for t in tasks:
+        print("\t{}".format(t))
